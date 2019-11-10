1aa5f4308fccfac360ed62e2218f2cff66efa6f6
Harold <harol.mont97@gmail.com>
2019-11-10 00:51:03 -0500

    Se agregan estilos

--- components/classComponent.js
+++ components/classComponent.js
@@ -1,14 +1,18 @@
-export default (props) =>(
-    <div>
-        <div>
-            <h2>Clases de {props.nombre}</h2>
-            <ul>
+export default (props) => (
+    
+        <div style={{display:"flex"}}>
+            <h2>Clases de {props.nombre}: </h2>
                 {props.clase.map(
-                    e =>{
-                        return  <li>{e.first_name}</li>
+                    e => {
+                        return (
+                            <div>
+                                <p>
+                                    <img src={e.foto} alt="Aqui va la foto si tuviera una"></img>
+                                    {e.first_name}
+                                </p>
+                            </div>
+                        )
                     }
                 )}
-            </ul>
         </div>
-    </div>
 )
